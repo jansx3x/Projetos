8fdb5dcd6d60e7526716e970997a49a0164a8378
Jane Araujo <noscared66@gmail.com>
2021-03-20 17:48:03 -0300

    criar projeto inicial

---
+++ imovel/imovel/src/main/java/br/com/ifma/imovel/ImovelApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifma.imovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImovelApplication.class, args);
+	}
+
+}
